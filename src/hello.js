fa1c6ece3101f35529c7b31027d676e47d742cd8
shen jialiang <shenjialiang993@gmail.com>
2022-04-26 11:47:00 +0900

    testing git add -p

---
+++ src/hello.js
@@ -0,0 +1,17 @@
+const hello = () => {
+    console.log("hello world");
+}
+//this is a comment
+// Ok shift + o is add before the line 
+// Ok o is add after the line (meaning changing the line)
+// hello();
+
+
+
+const add = (a, b) => {
+    console.log(a + b);
+}
+add(2,3)
+console.log("hello");
+console.log("why");
+console.log("me");
